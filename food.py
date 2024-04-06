9228efb3b2c3a9487c461283567985bf30fb5d7a
udisha0530 <135735575+udisha0530@users.noreply.github.com>
2024-04-07 02:11:34 +0530

    Update food.py

--- food.py
+++ food.py
@@ -225,14 +225,11 @@ def recommend_food(df_age, user_profile,kmeans):
     user_features = user_profile_df[features]
     
     # Print user_features for debugging
-    print("User Features:")
-    print(user_features)
-    
     # Predict the cluster for the user profile
     user_cluster = kmeans.predict(user_features)[0]
     
     # Filter food items belonging to the predicted cluster
-    recommended_food = df[df['cluster'] == user_cluster]['Food_items']
+    recommended_food = df_age[df_age['cluster'] == user_cluster]['Food_items']
     
     return recommended_food
 def filter(user_series_dietary,user_series_nutrient,df_new):
